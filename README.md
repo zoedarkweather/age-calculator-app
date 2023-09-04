b28e90a1a2aafc9c796c78fbfcdf8b446f980329
Jennifer Souza <141974519+zoedarkweather@users.noreply.github.com>
2023-09-03 20:15:04 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# age-calculator-app
-Mobile-first age-calculator  app solution using CSS grid and JavaScript
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+
+### Screenshot
+
+![](./screenshot.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+
+
+### What I learned
+
+I learned that working with dates in JavaScript is more challenging than I'd anticipated. Date() has some unusual behavior. For example, any date that is 31 or less is accepted and made to be a date in the following month. For example, if April 31 is given to Date() it will return May 1 rather than invalid date. Also, months are zero-based, but if you give Date() a string in the format "MM/DD/YYY" you don't need to adjust for that. If you feed it integers, it doesn't always return the expected date even if you account for the zero-based months.  
+
+
+### Continued development
+ 
+ Keep practicing everything. Also, play with dates in JavaScript more - I feel like I'm at least expecting some strange behavior from Date() now, but I'd like to learn some more tricks in dealing with this.
+
+### Useful resources
+
+- [https://stackoverflow.com/questions/21188420/javascript-date-validation-not-validation-february-31](https://stackoverflow.com/questions/21188420/javascript-date-validation-not-validation-february-31) - This helped me resolve the anything 31 days or less is a valid date issue.
+
+- [https://stackoverflow.com/questions/12254333/javascript-is-creating-date-wrong-month](https://stackoverflow.com/questions/12254333/javascript-is-creating-date-wrong-month) - This helped me learn that months are zero-based and how to get around that.
+
+## Author
+
+- Github - [Jennifer Souza](https://github.com/zoedarkweather)
+- Frontend Mentor - [@zoedarkweather](https://www.frontendmentor.io/profile/zoedarkweather)
+
+
---
+++ age-calculator.js
@@ -0,0 +1,112 @@
+const form = document.querySelector("form");
+const dateInputs = document.querySelectorAll("input");
+
+for (const dateInput of dateInputs) {
+    dateInput.addEventListener("focus", (e) => {
+        let inputID = e.target.getAttribute("id");   
+        clearError(inputID);
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let day = "";
+    let month = "";
+    let year = "";
+    
+    for (const dateInput of dateInputs) {        
+        const inputID = dateInput.getAttribute("id");
+        clearError(inputID);
+
+        if (!dateInput.validity.valid) {
+            showError(inputID);
+        } else if (isNaN(dateInput.value)) {
+            showError(inputID, "Please enter a number");
+        } else {
+            if (parseInt(dateInput.value) <= 0) {
+                showError(inputID);
+            }
+
+            switch (inputID) {
+                case "day":
+                    day = dateInput.value;
+                    if (parseInt(day) > 31) {
+                        showError(inputID);
+                    }
+                    break;
+                case "month":
+                    month = dateInput.value;
+                    if (parseInt(month) > 12) {
+                        showError(inputID);
+                    }
+                    break;
+                case "year":
+                    year = dateInput.value;
+                    const today = new Date()
+                    if (parseInt(year) > today.getFullYear()) {
+                        showError(inputID, "Must be in the past")
+                        return;
+                    }
+                    break;
+                default:
+                    break;                    
+            }
+        }        
+    }
+    
+    const dateEntered = new Date(`${month}/${day}/${year}`); 
+
+    //If an invalid date with a day < 32 is entered,  
+    //JS will make it a valid date in the next month.
+    //This checks to see if the date entered doesn't 
+    //== the date created by the Date constructor, and
+    //makes such a date invalid.
+    if (dateEntered.getDate() !== parseInt(day)) {
+        showError("day", "Must be a valid date");                  
+    } else {
+        calculateAge(dateEntered)      
+    }       
+});
+
+function showError(inputID, errorMsg = "") {
+    const dateInput = document.querySelector(`#${inputID}`); 
+    const errorPara = document.querySelector(`#${inputID} + p`);
+
+    if (errorMsg === "") {            
+  
+        if (dateInput.validity.valueMissing) {
+            errorMsg = "This field is required";
+        } else if (dateInput.validity.tooShort) {
+            errorMsg = `Please enter a ${inputID} in the correct format`;
+        } else {
+            errorMsg = `Must be a valid ${inputID}`;
+        }
+    }
+
+    errorPara.textContent = errorMsg;
+    dateInput.setAttribute("class", `date-input ${inputID} input-error`);
+}
+
+function clearError(inputID) {
+    const dateInput = document.querySelector(`#${inputID}`);  
+    const errorPara = document.querySelector(`#${inputID} + p`);
+    errorPara.textContent = "";
+    dateInput.setAttribute("class", `date-input ${inputID}`);
+}
+
+function calculateAge(dateEntered) { 
+    const today = new Date();
+    const age = today - dateEntered;
+
+    const years = Math.floor(age/31556952000);
+    const months = Math.floor((age - years * 31556952000) / 2629746000);
+    const days = Math.floor((age - years * 31556952000 - months * 2629746000) / 86400000);
+
+    const yearOutput = document.querySelector(".age-years");
+    const monthOutput = document.querySelector(".age-months");
+    const dayOutput = document.querySelector(".age-days");
+
+    yearOutput.textContent = years;
+    monthOutput.textContent = months;
+    dayOutput.textContent = days;
+}
